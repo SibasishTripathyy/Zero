416d30f5ddf8692a4c501149a8e7698b9e008920
Sibasish Tripathyy <sibasish.tripathyy@gmail.com>
2022-07-09 13:07:21 +0530

    API Creation: Add Customer Address

--- backend/CustomerService/src/main/java/com/sibasish/ecom/customerservice/service/CustomerService.java
+++ backend/CustomerService/src/main/java/com/sibasish/ecom/customerservice/service/CustomerService.java
@@ -1,5 +1,6 @@
 package com.sibasish.ecom.customerservice.service;
 
+import com.sibasish.ecom.customerservice.request.CustomerAddressRequest;
 import com.sibasish.ecom.customerservice.request.CustomerRequest;
 import com.sibasish.ecom.customerservice.response.CustomerResponse;
 
@@ -9,4 +10,6 @@ public interface CustomerService {
     CustomerResponse createUser(CustomerRequest customerRequest);
 
     List<CustomerResponse> getAllCustomers();
+
+    String addAddress(CustomerAddressRequest customerAddressRequest);
 }
