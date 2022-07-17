0ccc012afdeaea1f445cdbfedeccf349583e2cfb
Sibasish Tripathyy <sibasish.tripathyy@gmail.com>
2022-07-18 00:22:40 +0530

    Added Global Exception Handling for product service

---
+++ backend/ProductService/src/main/java/com/sibasish/ecom/productservice/exceptions/NoDataFoundException.java
@@ -0,0 +1,8 @@
+package com.sibasish.ecom.productservice.exceptions;
+
+public class NoDataFoundException extends RuntimeException {
+
+    public NoDataFoundException(String message) {
+        super(message);
+    }
+}
---
+++ backend/ProductService/src/main/java/com/sibasish/ecom/productservice/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.sibasish.ecom.productservice.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
