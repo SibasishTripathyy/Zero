a4f2a852c2c084bf6d290157a9aec0fc63fae9a5
Sibasish Tripathyy <sibasish.tripathyy@gmail.com>
2022-07-17 12:30:27 +0530

    Added product microservice

---
+++ backend/ProductService/src/test/java/com/sibasish/ecom/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sibasish.ecom.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
