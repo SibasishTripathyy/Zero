1b237be1fe1697bcfdee4162808fe8669243b7ac
Sibasish Tripathyy <sibasish.tripathyy@gmail.com>
2022-07-19 02:44:54 +0530

    Added "Create Category" API.

--- backend/ProductService/src/main/java/com/sibasish/ecom/productservice/controller/CategoryController.java
+++ backend/ProductService/src/main/java/com/sibasish/ecom/productservice/controller/CategoryController.java
@@ -1,13 +1,12 @@
 package com.sibasish.ecom.productservice.controller;
 
+import com.sibasish.ecom.productservice.request.CategoryRequest;
 import com.sibasish.ecom.productservice.response.CategoryResponse;
 import com.sibasish.ecom.productservice.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +16,14 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @PostMapping("/create")
+    public ResponseEntity<CategoryResponse> createCategory(@RequestBody CategoryRequest categoryRequest) {
+
+        CategoryResponse categoryResponse = categoryService.createCategory(categoryRequest);
+
+        return new ResponseEntity<>(categoryResponse, HttpStatus.CREATED);
+    }
 
     @GetMapping("/all")
     public ResponseEntity<List<CategoryResponse>> getAllCategories() {
--- backend/ProductService/src/main/java/com/sibasish/ecom/productservice/service/impl/CategoryServiceImpl.java
+++ backend/ProductService/src/main/java/com/sibasish/ecom/productservice/service/impl/CategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.sibasish.ecom.productservice.service.impl;
 import com.sibasish.ecom.productservice.entity.Category;
 import com.sibasish.ecom.productservice.exceptions.NoDataFoundException;
 import com.sibasish.ecom.productservice.repository.CategoryRepository;
+import com.sibasish.ecom.productservice.request.CategoryRequest;
 import com.sibasish.ecom.productservice.response.CategoryResponse;
 import com.sibasish.ecom.productservice.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +11,27 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @Override
+    public CategoryResponse createCategory(CategoryRequest categoryRequest) {
+
+        Category category = Category.builder()
+                .name(categoryRequest.getCategoryName().toLowerCase(Locale.ROOT))
+                .build();
+
+        category = categoryRepository.save(category);
+
+        return CategoryResponse.builder()
+                .categoryName(category.getName())
+                .build();
+    }
 
     @Override
     public List<CategoryResponse> getAllCategories() {
