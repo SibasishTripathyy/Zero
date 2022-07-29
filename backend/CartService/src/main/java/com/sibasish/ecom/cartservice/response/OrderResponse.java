61b7d5f525e43469503ce5fc9c654008ff1d58ae
Sibasish Tripathyy <sibasish.tripathyy@gmail.com>
2022-07-29 19:06:52 +0530

    Added purchase from cart feature.

---
+++ backend/CartService/src/main/java/com/sibasish/ecom/cartservice/response/OrderResponse.java
@@ -0,0 +1,25 @@
+package com.sibasish.ecom.cartservice.response;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class OrderResponse {
+
+    private UUID orderId;
+    private Double totalPrice;
+    private String shippingAddress;
+    private String paymentMethod;
+    private LocalDate orderDate;
+
+    private List<OrderItemResponse> orderItemResponseList;
+
+    private PaymentResponse paymentResponse;
+}
